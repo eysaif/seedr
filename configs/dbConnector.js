9472ad3775cc9fa4459c46c507de75bdf0dfece9
shaikh Saifuddin <u4saif@gmail.com>
2024-03-27 20:11:48 +0530

    db connection added

---
+++ configs/dbConnector.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+  try {
+    const connectionStatus = await mongoose.connect(process.env.MONGO_URL, {
+      dbName: "simplyHire",
+    });
+    console.log(
+      `DataBase Connected Successfully. 🚨PORT:${connectionStatus.connection.port}`
+    );
+  } catch (error) {
+    console.log(`Unable to connect DB${error}`);
+  }
+};
+
+export { dbConnection };
--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import seedrRoutes from "./routes/seedrRoute.js";
+import { dbConnection } from "./configs/dbConnector.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// CORS is enabled for all origins
+/**
+ * CORS is enabled for all origins
+ */
 app.use(cors());
 
-//TO READ THE REQUEST BODY
-app.use(express.json());
+/**
+ * Configure ENV Path
+ */
+dotenv.config({ path: "./configs/config.env" });
+
+/**
+ * Database Conection
+ */
+dbConnection();
+
+/**
+ * TO READ THE REQUEST BODY
+ */
+app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 /**
